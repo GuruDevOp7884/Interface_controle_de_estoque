60d053a8db2890e58e6b4ea2c77310f66bccd2cf
GuruDevOp <gugaalves92@hotmail.com>
2022-08-28 23:27:24 -0300

    update

--- main.js
+++ main.js
@@ -17,8 +17,11 @@ function adicionar()
         var celula_descricao = nova_celula.insertCell(4)
 
         celula_nome.innerHTML = produto[0].toUpperCase() + produto.slice(1) //<coloca a primeira letra maiuscula
+
         celula_quantidade.innerHTML = quantidade[0].toUpperCase() + quantidade.slice(1)
+
         celula_descricao.innerHTML = descricao[0].toUpperCase() + descricao.slice(1)
+        
         celula_codigo.innerHTML = "#" + codigo
 
         celula_item.style.fontWeight = '700'
@@ -37,13 +40,13 @@ function adicionar()
     }
 }
 
-//incrementa ao adicionar item
+//incrementa quantidade de itens ao adicionar produto
     var numero = 5;
 
 function remover()
 {
     var tabela = document.getElementById('estoque').deleteRow(0)
-    var novaTabela = nova_celula.deleteRow(0)
+    numero--
 }
 
 
